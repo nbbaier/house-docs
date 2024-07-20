ddddb949f4f96af3451a7255ccd963fca4e3486c
Nico Baier <nico.baier@gmail.com>
2024-07-19 20:20:01 -0500

    style(prettier): update prettier configuration for mdx files

--- prettierrc.config.mjs
+++ prettierrc.config.mjs
@@ -1,16 +1,15 @@
-// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
-
 /**
  * @see https://prettier.io/docs/en/configuration.html
  * @type {import("prettier").Config}
  */
 const config = {
-	semi: false,
 	overrides: [
 		{
 			files: "*.mdx",
 			options: {
 				parser: "mdx",
+				semi: false,
+				htmlWhitespaceSensitivity: "ignore",
 			},
 		},
 	],
